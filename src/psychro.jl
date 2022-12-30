43d71bcda54a38ebd27358cca5fdf2be290edc10
Alexandre Umpierre <105897054+aumpierre-unb@users.noreply.github.com>
2022-12-30 19:32:09 -0300

    doc strings

--- src/psychro.jl
+++ src/psychro.jl
@@ -118,30 +118,25 @@ Assume the amount of dry air is constant.
 # The initial condition is
 Tdry1=293;
 Twet1=288;
-Tdry,Twet,Tdew,Tadiab,W,Wsat,Wsatwet,Wadiab,h,v,phi,pw,psat,psatwet,rho=
-psychro(Tdry=Tdry1,Twet=Twet1,fig=true)
+Tdry,Twet,Tdew,Tadiab,W,Wsat,Wsatwet,Wadiab,h,v,phi,pw,psat,psatwet,rho=psychro(Tdry=Tdry1,Twet=Twet1,fig=true)
 
 # The thermodynamic state the after first adiabatic saturation is
 Tdry2=323;
 W2=W1;
-Tdry,Twet,Tdew,Tadiab,W,Wsat,Wsatwet,Wadiab,h,v,phi,pw,psat,psatwet,rho=
-psychro(Tdry=Tdry2,W=W2,fig=true)
+Tdry,Twet,Tdew,Tadiab,W,Wsat,Wsatwet,Wadiab,h,v,phi,pw,psat,psatwet,rho=psychro(Tdry=Tdry2,W=W2,fig=true)
 # The thermodynamic state the after first adiabatic saturation is
 h3=h2;
 Tdry3,W3=adiabSat(h3)
-Tdry,Twet,Tdew,Tadiab,W,Wsat,Wsatwet,Wadiab,h,v,phi,pw,psat,psatwet,rho=
-psychro(Tdry=Tdry3,W=W3)
+Tdry,Twet,Tdew,Tadiab,W,Wsat,Wsatwet,Wadiab,h,v,phi,pw,psat,psatwet,rho=psychro(Tdry=Tdry3,W=W3)
 
 # The thermodynamic state after the second heating is
 Tdry4=323;
 W4=W3;
-Tdry,Twet,Tdew,Tadiab,W,Wsat,Wsatwet,Wadiab,h,v,phi,pw,psat,psatwet,rho=
-psychro(Tdry=Tdry4,W=W4,fig=true)
+Tdry,Twet,Tdew,Tadiab,W,Wsat,Wsatwet,Wadiab,h,v,phi,pw,psat,psatwet,rho=psychro(Tdry=Tdry4,W=W4,fig=true)
 # The thermodynamic state the after second adiabatic saturation is
 h5=h4;
 Tdry5,W5=adiabSat(h5)
-Tdry,Twet,Tdew,Tadiab,W,Wsat,Wsatwet,Wadiab,h,v,phi,pw,psat,psatwet,rho=
-psychro(Tdry=Tdry5,W=W5)
+Tdry,Twet,Tdew,Tadiab,W,Wsat,Wsatwet,Wadiab,h,v,phi,pw,psat,psatwet,rho=psychro(Tdry=Tdry5,W=W5)
 
 # The energy and water vapor demands are
 (h5-h1)*(8.5/v1) # demand of energy
