3c7209495383234369bbc8b420eee497cf605d00
Alexandre Umpierre <105897054+aumpierre-unb@users.noreply.github.com>
2024-09-05 11:40:11 -0300

    Update doPlot.jl

--- src/doPlot.jl
+++ src/doPlot.jl
@@ -141,7 +141,6 @@ function doPlot()
     path = Base.find_package("Psychrometrics")
     file = string(path[1:length(path)-length("src/Psychrometrics.jl")], "\\julia-logo-color.png")
     img = load(file)
-    # img = load("https://github.com/aumpierre-unb/Psychrometrics.jl/raw/main/julia-logo-color.png")
     plot!([274.5, 283.5], [33.2e-3, 36.2e-3],
         reverse(img, dims=1),
         yflip=false,
