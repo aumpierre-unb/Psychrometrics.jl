244bf980d83334aa5f57a85562dbebdad0d7b64b
Alexandre Umpierre <aumpierre@gmail.com>
2024-09-06 22:36:36 -0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,34 @@
 using Psychrometrics
 using Test
-using Plots
 
 @testset "Psychrometrics.jl" begin
-    # data(x) = x .^ 1.11 .* (1 - x) .^ 1.09 + x
-    # x = [0.1 0.5 0.9]
-    # q = 0.54
-    # R = refmin(data, x, q)
-    # R = 1.7 * R
-    # @test 9 > strays(data, x, q, R, true, false) > 8
+    @test abs(
+        Psychrometrics.satPress(
+            25 + 273.15;
+        ) - 3169
+    ) < 1
+    @test abs(
+        Psychrometrics.volume(
+            25 + 273.15,
+            7e-3
+        ) - 0.8541
+    ) < 1e-4
+    @test abs(
+        Psychrometrics.enthalpy(
+            25 + 273.15,
+            7e-3
+        ) - 42982
+    ) < 1
+    @test abs(
+        Psychrometrics.dewTemp(
+            1e3
+        ) - 280.14689999999996
+    ) < 1
+    @test all(
+        abs.(
+            Psychrometrics.adiabSat(
+                82.4e3
+            ) .- (299, 0.0218)
+        ) .< (1, 1e-4)
+    )
 end
